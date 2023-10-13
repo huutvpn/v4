35168f6b66ab82db4a93e7910da0023740f1eb4b
huutvpn <143250398+huutvpn@users.noreply.github.com>
2023-10-14 04:03:05 +0700

    Add files via upload

--- udp/udp.sh
+++ udp/udp.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
-#Script UdpCustom 2023
-#Script By MARDHEX
-
+# color
+BGreen='\e[1;32m'
+NC='\e[0m'
 cd
 rm -rf /root/udp
 mkdir -p /root/udp
 # install udp-custom
 echo ""
-sleep 4
-echo " install UDP-CUSTOM........" | lolcat
-sleep 4
+sleep 1
+echo "\e[1;32m Proses Download Script UdpCustom.. \e[0m"
+sleep 1
 clear
-echo "OTW BARENG SETANVPN........" | lolcat
+echo "\e[1;32m Cecking Tool UdpCustom.... \e[0m"
+sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://gitlab.com/Exe302/Tunnel/-/raw/main/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+echo "\e[1;32m Succes Cecking Tool.. \e[0m"
+sleep 1
+clear
+echo "\e[1;32m Please Waiting Proses Downloading Toll UdpCustom.. \e[0m"
+sleep 1
+clear
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
 clear
 # install Config Default Udp
 echo ""
-echo "Mempersiapkan progres udp......." | lolcat
-sleep 4
+sleep 1
+echo "\e[1;32m Proses Download Script Config Default.. \e[0m"
+sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://gitlab.com/Exe302/Tunnel/-/raw/main/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+echo "\e[1;32m Cecking Config Default By Fv Stores.. \e[0m"
+sleep 1
+clear
+echo "\e[1;32m Succes Cecking Config Default Tool.. \e[0m"
+sleep 1
+clear
+echo "\e[1;32m Please Waiting Proses Downloading Default Config UdpCustom.. \e[0m"
+sleep 1
+clear
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
 chmod 644 /root/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by Andyyuda
+Description=UDP Custom by Fv Stores
 
 [Service]
 User=root
@@ -43,7 +60,7 @@ EOF
 else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by MARDHEX
+Description=UDP Custom by Fv Stores
 
 [Service]
 User=root
@@ -67,4 +84,8 @@ systemctl enable udp-custom &>/dev/null
 echo ""
 sleep 0,5
 clear
+rm -rf /usr/bin/udp
+echo -e "\e[1;32m auto reboot in 5s \e[0m"
+sleep 5
+reboot
 
